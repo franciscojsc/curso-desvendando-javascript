364698759e6194713a6be1b3e3d88ba89947a527
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-25 13:26:17 -0300

    Tipos de variáveis

---
+++ aula01.js
@@ -0,0 +1,11 @@
+//Tipos de variáveis
+
+var name = "AgileCode";
+var top10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var $scope = { title: "Javascript", version: "6" };
+var _person = "John";
+var numberCode = 8989992341;
+var a = 10,
+    b = 9,
+    c = 8;
+var language;
