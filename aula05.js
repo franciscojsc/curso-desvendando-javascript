db514b5ec91205e6573323c33db69969bc864a73
Francisco Chaves <francisco.jschaves@gmail.com>
2019-02-25 14:09:36 -0300

    undefined e null
    undefined - caso uma propiedade de um determinado objeto seja consultado e não exista.
    null -  indica a ausência de valor em uma determinada propiedade já existente.

---
+++ aula05.js
@@ -0,0 +1,7 @@
+//undefined - caso uma propiedade de um determinado objeto seja consultado e não exista.
+
+var pessoa = {};
+pessoa.idade; //undefined
+
+// null -  indica a ausência de valor em uma determinada propiedade já existente.
+var carro = { marca: 'FIAT', placa: null };
